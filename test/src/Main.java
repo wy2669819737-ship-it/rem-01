8d5f0f277e0c73a4747b913912a431312eac759e
wy2669819737-ship-it <wy2669819737@163.com>
2026-08-10 16:19:38 +0800

    Update Main.java

--- test/src/Main.java
+++ test/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("Hello and welcome!");
         System.out.println("test");
+        System.out.printf("Hello and welcome!");
+        System.out.println("test");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
